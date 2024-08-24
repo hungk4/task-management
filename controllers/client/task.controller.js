0e93f6087595710baf0084f05b6720be2e9e2d38
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-08-24 16:42:44 +0700

    loc theo trang thai va sap xep

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -3,9 +3,28 @@ const Task = require("../../models/task.model");
 
 // [GET] /tasks
 module.exports.index =  async (req, res) => {
-  const tasks = await Task.find({
+  const find = {
     deleted: false
-  });
+  };
+  // Lọc theo trạng thái
+  const status = req.query.status;
+  if(status){
+    find.status = status;
+  }
+  // Hết Lọc theo trạng thái
+
+    // Sắp xếp
+    const sort = {};
+
+    const sortKey = req.query.sortKey;
+    const sortValue = req.query.sortValue;
+  
+    if(sortKey && sortValue) {
+      sort[sortKey] = sortValue;
+    }
+    // Hết Sắp xếp
+
+  const tasks = await Task.find(find);
 
   res.json(tasks);
 };
