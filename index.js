b126c0541ff1e65a13affa1f58e053c1813fa3ae
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-08-27 17:54:30 +0700

    cai dat cors

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ database.connect();
 const app = express();
 const port = process.env.PORT;
 
+// CORS
+const cors = require("cors");
+// CORS
+// Cách 1: Tất cả tên miền được phép truy cập
+app.use(cors());
+
+// Cách 2: Áp dụng cho 1 tên miền cụ thể
+// const corsOptions = {
+//   origin: 'https://abc.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+// End CORS
+
 // parse application/json
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
