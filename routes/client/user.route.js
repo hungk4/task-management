04861a390793f73686263fb92e0e54b3109859b4
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-08-29 16:39:33 +0700

    reset mat khau

--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -10,4 +10,7 @@ router.post("/login", controller.login);
 router.post("/password/forgot", controller.forgotPassword);
 
 router.post("/password/otp", controller.otpPassword);
+
+router.patch("/password/reset", controller.resetPassword);
+
 module.exports = router;
